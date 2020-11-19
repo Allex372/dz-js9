354fb6c5862f8bea1e3235777d2f69b6c21ac33b
Allex372 <62441924+Allex372@users.noreply.github.com>
2020-11-20 00:27:22 +0200

    dz

--- index.js
+++ index.js
@@ -11,133 +11,130 @@
 //     Якщо щось пішло не так (нема шо їсти), то має бути викинута помилка і решта функцій виконуватись не мають.
 //     Якщо ж все ок, то ви маєте прожити свій звичайний день.
 //     Кожна подія має бути з рандомною (не по зростанню) затримкою.
-
-function myDay(time , taskStart){
-    console.log('привіт мій друже! дякую що завітав')
-    console.log('Зараз я розкажу тобі про свій день!')
-
-    setTimeout(()=>{
-        console.log('DAY START')
-        if (time > 8){
-            taskStart('проспав',null);
-            return;
-        }
-         taskStart( null, 'прокинувся вчасно, можна спокійно вставати');
-    }, 2000)
-}
-
-function bathroom (day, cb){
-    setTimeout(()=>{
-        if (day === 'bad'){
-            cb(false, 'НАДЗВИЧАЙНО ЩВИДКО');
-            return
-        }
-        cb(true, 'Спокійно вмиваюся, та чищу зуби.');
-    },1000)
-}
-
-
-function breakfast(day,cb){
-    setTimeout(()=>{
-        if (day === 'bad'){
-            cb(false,'AAAAAAAAAAAA')
-        } else {
-            cb(true,'готую сніданок ')
-        }
-
-    },3000)
-}
-
-function goJob(day,cb){
-    setTimeout(()=>{
-        if (day === 'bad'){
-            cb(false,'AAAA')
-        }else {
-            cb(true, 'добираюся на роботку')
-        }
-    },500)
-}
-function work(day,cb){
-    setTimeout(()=>{
-        if (day==='bad'){
-            cb(false,'AAAA')
-        }else {
-            cb(true, 'встигну');
-        }
-    },3000)
-}
-function dinner(day,cb){
-    setTimeout(()=>{
-        if (day==='bad'){
-            cb(false,'AAAA')
-        }else {
-            cb(true, 'неймовірно смачний')
-        }
-    }, 500)
-}
-function hoHome(day,cb){
-    setTimeout(()=>{
-        if (day==='bad'){
-            cb(false,'AAA')
-        }else {
-            cb(true, 'з радості ')
-        }
-    }, 2000)
-}
-
-myDay(7, (error, standUp) => {
-    if (error){
-        console.log('Мля я проспав треба летіти');
-    }else {
-        console.log(`${standUp} з ліжка`)
-    }
-
-   bathroom('', (err, text) => {
-       if(!err) {
-           console.log(text + ' ЧИЩУ ЗУБИ');
-       } else {
-           console.log(text);
-       }
-        breakfast('',(err, data)=> {
-            if (!err) {
-                console.log('Все пропало не встигаю снідати');
-            } else {
-                console.log(data + 'Можна ще новини з кавою почитати')
-            }
-            goJob('',(err,data) =>{
-                    if (!err){
-                        console.log('Фанарь я запізнився на автобус, треба йти пішки')
-                    }else {
-                        console.log('Під музичку чілю та ' +  data)
-                    }
-                    work('',(err,data)=>{
-                        if (!err){
-                            console.log('НІЧОго НЕ ВСТИГАЮ')
-                        }else {
-                            console.log(`Сьогодні все ${data} зробити:)`)
-                        }
-                        dinner('',(err, data)=>{
-                            if(!err){
-                                console.log('НЕ ВСТИГАЮ ОБІДАТИ....ЦЕ КАПЕЦЬ')
-                            }else {
-                                console.log(`Сьогодні обід ${data}`)
-                            }
-                            hoHome('',(err,data)=>{
-                                if (!err){
-                                    console.log('НАП*ЮСЯ З ГОРЯ')
-                                }else {
-                                    console.log(`${data} навалюся)`)
-                                }
-                                console.log('DAY OFFF)))))))')
-                            })
-                        })
-                    })
-                })
-            })
-        })
-});
-
-
+// function myDay(time , taskStart){
+//     console.log('привіт мій друже! дякую що завітав')
+//     console.log('Зараз я розкажу тобі про свій день!')
+//
+//     setTimeout(()=>{
+//         console.log('DAY START')
+//         if (time > 8){
+//             taskStart('проспав',null);
+//             return;
+//         }
+//          taskStart( null, 'прокинувся вчасно, можна спокійно вставати');
+//     }, 2000)
+// }
+//
+// function bathroom (day, cb){
+//     setTimeout(()=>{
+//         if (day === 'bad'){
+//             cb(false, 'НАДЗВИЧАЙНО ЩВИДКО');
+//             return
+//         }
+//         cb(true, 'Спокійно вмиваюся, та чищу зуби.');
+//     },1000)
+// }
+//
+//
+// function breakfast(day,cb){
+//     setTimeout(()=>{
+//         if (day === 'bad'){
+//             cb(false,'AAAAAAAAAAAA')
+//         } else {
+//             cb(true,'готую сніданок ')
+//         }
+//
+//     },3000)
+// }
+//
+// function goJob(day,cb){
+//     setTimeout(()=>{
+//         if (day === 'bad'){
+//             cb(false,'AAAA')
+//         }else {
+//             cb(true, 'добираюся на роботку')
+//         }
+//     },500)
+// }
+// function work(day,cb){
+//     setTimeout(()=>{
+//         if (day==='bad'){
+//             cb(false,'AAAA')
+//         }else {
+//             cb(true, 'встигну');
+//         }
+//     },3000)
+// }
+// function dinner(day,cb){
+//     setTimeout(()=>{
+//         if (day==='bad'){
+//             cb(false,'AAAA')
+//         }else {
+//             cb(true, 'неймовірно смачний')
+//         }
+//     }, 500)
+// }
+// function hoHome(day,cb){
+//     setTimeout(()=>{
+//         if (day==='bad'){
+//             cb(false,'AAA')
+//         }else {
+//             cb(true, 'з радості ')
+//         }
+//     }, 2000)
+// }
+//
+// myDay(7, (error, standUp) => {
+//     if (error){
+//         console.log('Мля я проспав треба летіти');
+//     }else {
+//         console.log(`${standUp} з ліжка`)
+//     }
+//
+//    bathroom('', (err, text) => {
+//        if(!err) {
+//            console.log(text + ' ЧИЩУ ЗУБИ');
+//        } else {
+//            console.log(text);
+//        }
+//         breakfast('',(err, data)=> {
+//             if (!err) {
+//                 console.log('Все пропало не встигаю снідати');
+//             } else {
+//                 console.log(data + 'Можна ще новини з кавою почитати')
+//             }
+//             goJob('',(err,data) =>{
+//                     if (!err){
+//                         console.log('Фанарь я запізнився на автобус, треба йти пішки')
+//                     }else {
+//                         console.log('Під музичку чілю та ' +  data)
+//                     }
+//                     work('',(err,data)=>{
+//                         if (!err){
+//                             console.log('НІЧОго НЕ ВСТИГАЮ')
+//                         }else {
+//                             console.log(`Сьогодні все ${data} зробити:)`)
+//                         }
+//                         dinner('',(err, data)=>{
+//                             if(!err){
+//                                 console.log('НЕ ВСТИГАЮ ОБІДАТИ....ЦЕ КАПЕЦЬ')
+//                             }else {
+//                                 console.log(`Сьогодні обід ${data}`)
+//                             }
+//                             hoHome('',(err,data)=>{
+//                                 if (!err){
+//                                     console.log('НАП*ЮСЯ З ГОРЯ')
+//                                 }else {
+//                                     console.log(`${data} навалюся)`)
+//                                 }
+//                                 console.log('DAY OFFF)))))))')
+//                             })
+//                         })
+//                     })
+//                 })
+//             })
+//         })
+// });
 
 //
 // function orderTour(money, taskWhenReady) {
@@ -201,3 +198,138 @@ myDay(7, (error, standUp) => {
 //   }
 //   console.log('Єє бой. Берчдчиів !!!')
 // });
+
+function myDay(time){
+    return new Promise((resolve, reject)=>{
+        console.log('привіт мій друже! дякую що завітав')
+        console.log('Зараз я розкажу тобі про свій день!')
+
+        setTimeout(()=>{
+            console.log('DAY START')
+            if (time > 8){
+                resolve('проспав');
+                return;
+            }
+            reject( 'прокинувся вчасно, можна спокійно вставати');
+        }, 2000)
+    })
+
+}
+
+function bathroom (day){
+    return new Promise(((resolve, reject) => {
+        setTimeout(()=>{
+            if (day === 'bad'){
+                resolve('НАДЗВИЧАЙНО ЩВИДКО');
+                return
+            }
+            reject('Спокійно вмиваюся, та чищу зуби.');
+        },1000)
+    }))
+}
+function breakfast(day){
+    return new Promise(((resolve, reject) => {
+        setTimeout(()=>{
+            if (day === 'bad'){
+                resolve('AAAAAAAAAAAA')
+            } else {
+                reject('готую сніданок ')
+            }
+
+        },3000)
+    }))
+
+}
+
+function goJob(day,cb){
+    return new Promise(((resolve, reject) => {
+        setTimeout(()=>{
+            if (day === 'bad'){
+                resolve('AAAA')
+            }else {
+                reject('добираюся на роботку')
+            }
+        },500)
+    }))
+
+}
+function work(day,cb){
+    return new Promise(((resolve, reject) => {
+        setTimeout(()=>{
+            if (day==='bad'){
+                resolve('AAAA')
+            }else {
+                reject('встигну');
+            }
+        },3000)
+    }))
+
+}
+function dinner(day,cb){
+    return new Promise(((resolve, reject) => {
+        setTimeout(()=>{
+            if (day==='bad'){
+                resolve('AAAA')
+            }else {
+                reject('неймовірно смачний')
+            }
+        }, 500)
+    }))
+
+}
+function hoHome(day){
+    return new Promise(((resolve, reject) => {
+        setTimeout(()=>{
+            if (day==='bad'){
+                resolve('AAA')
+            }else {
+                reject('з радості ')
+            }
+        }, 2000)
+    }))
+
+}
+
+
+
+myDay(9)
+.then((data)=>{
+    console.log(`Бляха я  ${data}`)
+
+    return bathroom('bad')
+})
+.then(thoose=>{
+    console.log(thoose + ' ЧИЩУ ЗУБИ')
+
+    return breakfast('bad')
+})
+.then(brak=>{
+    console.log(brak+' Я НЕ ВСТИГАЮ ПОСНІДАТИ')
+
+    return goJob('')
+})
+.then(omg=>{
+    console.log(omg + ' Я НЕ ВСТИГ НА АВТОБУС')
+
+    return work('bad');
+})
+.then(workTime=>{
+    console.log(workTime+ ' Я не ВСТИГАЮ ВИКОНАТИ ТАСКУ')
+
+    return dinner('bad');
+})
+.then(eat=>{
+    console.log(eat + ' Я НЕ ВСТИГАЮ ПОЇСТИ')
+
+    return hoHome('bad');
+})
+.then(drink=>{
+    console.log(drink + ' НАП*ЮСЯ ЗІ ВСЬОГО')
+    console.log('DAY OFF ^-^')
+})
+.catch(err=>{
+    console.log('=================')
+    console.log(err)
+    console.log('=================')
+
+})
